975df1e2baa817fdb428cb2a5f2bb1704a4b56f6
[Mart]y <[stackm@usc.edu]>
2015-06-03 16:26:08 -0700

    updates to local fonts. Header is now working with all buttons on the right in a list. Header is now fixed.

--- 2.0/themes/digitallounge/header.php
+++ 2.0/themes/digitallounge/header.php
@@ -12,6 +12,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<style type="text/css">
+
+</style>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 
@@ -23,10 +26,23 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'digitallounge' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h1 class="site-title">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</h1> 
+			
 		</div><!-- .site-branding -->
+		<div id="navbuttons">
+			<ul>
+			<li><a href="#"><span class="chatwithus">chat with us</span></a></li>
+			<li><div class="binoculars"><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/binoc.svg" width="50" height="50"></a>
+			</div></li>
+			<li><div class="usclogo">
+				<a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/usclogo.svg" width="50" height="50"></a>
+			</div></li>
+			</ul>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
---
+++ 2.0/themes/digitallounge/js/calendarfunctions.js
@@ -0,0 +1,54 @@
+
+//Makes Calendar Icon Display Current Date- M.Stack 
+// Date Variable, all functions below rely on this 
+var d = new Date();
+
+//Declares all of the months in the year
+  function getMonthOfYear() {
+      var month = new Array();
+      month[0] = "January";
+      month[1] = "February";
+      month[2] = "March";
+      month[3] = "April";
+      month[4] = "May";
+      month[5] = "June";
+      month[6] = "July";
+      month[7] = "August";
+      month[8] = "September";
+      month[9] = "October";
+      month[10] = "November";
+      month[11] = "December";
+
+      var m = month[d.getMonth()];
+      document.getElementById("month").innerHTML = m;
+  }
+
+  // Pulls the day of the week Sun-Sat
+  function getDayOfWeek() {
+      var weekday = new Array(7);
+      weekday[0] = "Sunday";
+      weekday[1] = "Monday";
+      weekday[2] = "Tuesday";
+      weekday[3] = "Humpday"; //Why Not?
+      weekday[4] = "Thursday";
+      weekday[5] = "Friday";
+      weekday[6] = "Saturday";
+
+      var w = weekday[d.getDay()];
+      document.getElementById("dayofweek").innerHTML = w;
+  }
+ // Pulls the date numeber in the month 0-31
+    function getDayOfMonth() {
+      var n = d.getDate()
+      document.getElementById("daynumber").innerHTML = n;
+
+  }
+    //Tells DOM to fire off Calendar Functions after DOM is ready, uses Jquery
+
+    $( document ).ready(function() {
+  
+      getDayOfMonth();
+      getMonthOfYear();
+      getDayOfWeek();
+});
+ 

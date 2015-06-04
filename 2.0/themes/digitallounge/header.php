79bd483017c2c61fdb8596ed0587e2ad9988fcf1
[Mart]y <[stackm@usc.edu]>
2015-06-03 18:31:36 -0700

    just styling out the event readout now

--- 2.0/themes/digitallounge/header.php
+++ 2.0/themes/digitallounge/header.php
@@ -12,6 +12,8 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/calendarfunctions.js"></script>
 <style type="text/css">
 
 </style>
@@ -26,7 +28,21 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'digitallounge' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-
+		<div>
+			<time id="calendaricon" datetime="2014-09-20" class="icon">
+			  <em id="dayofweek"></em>
+			  <strong id="month"></strong>
+			  <span id="daynumber"></span>
+			</time>
+		</div>
+		<div id="eventreadout">
+			<ul>
+				<li><strong>1</strong><sub>pm</sub></li>
+				<li><span>Bluhost Intro<span></li>
+				<li><em>A new look at Wordpre...</em></li>
+			</ul>
+			
+		</div>
 		<div class="site-branding">
 			<h1 class="site-title">
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>

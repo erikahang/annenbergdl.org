2046ce5dfce0fffb5026fe9e66b6298fc02cd0fc
[Mart]y <[stackm@usc.edu]>
2015-06-09 17:22:05 -0700

    removed the html list in the footer, so footer menu will work correctly. Added a list of buttons for social links

--- 2.0/themes/digitallounge/footer.php
+++ 2.0/themes/digitallounge/footer.php
@@ -12,15 +12,14 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<ul>
+			<li class="facebook"><a href="#" alt="facebook"></a></li>
+			<li class="instagram"><a href="#" alt="instagram"></a></li>
+			<li class="twitter"><a href="#" alt="twitter"></a></li>
+			<li class="youtube"><a href="#" alt="youtube"></a></li>
+			</ul> 
 			<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
 
-			<ul>
-			<li><a href="#">About Us |</a></li>
-			<li><a href="#">Meet The Team |</a></li>
-			<li><a href="#">Our Space |</a></li>
-			<li><a href="#">ANN301 D, Digital Lounge, Wallis Annenberg Hall, USC|</a></li>
-			<li><a href="#">Open Mon - Thur, 9 am - 10PM, Friday 9am - 5pm |</a></li>
-			</ul>
 		</nav><!-- #site-navigation -->
 		<div class="site-info">
 			<?php echo get_theme_mod( 'footer_text', '' ); ?>

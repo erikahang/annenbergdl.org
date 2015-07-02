23b88fe56a46cfd973c9d781124ab8cb2eb2cf23
Nick Halsey <info@celloexpressions.com>
2015-07-01 21:42:29 -0700

    Load page data to JS to be used to query the next set of posts via ajax

--- 2.0/themes/digitallounge/js/infinite-scroll.js
+++ 2.0/themes/digitallounge/js/infinite-scroll.js
@@ -3,33 +3,62 @@
 
 	$(document).ready( function() {
 		bindEvents();
-		container = $( '#main' );
+		container = $( '.query-container' );
 		template = wp.template( 'archive-grid-view' );
-		args = {};
-		args.type = container.data( 'type' ); // todo
+		args = {
+			type: container.data( 'type' )
+		};
+		switch ( container.data( 'type' ) ) {
+			case 'post_type': 
+				args.post_type = container.data( 'post_type' );
+				break;
+			case 'taxonomy': 
+				args.taxonomy = container.data( 'taxonomy' );
+				args.term = container.data( 'term' );
+				break;
+			case 'post_type': 
+				args.post_type = container.data( 'post_type' );
+				break;
+			case 'author': 
+				args.author_id = container.data( 'author_id' );
+				break;
+			case 'day':
+				args.day = container.data( 'day' );
+				args.month = container.data( 'month' );
+				args.year = container.data( 'year' );
+				break;
+			case 'month':
+				args.month = container.data( 'month' );
+				args.year = container.data( 'year' );
+				break;
+			case 'year':
+				args.year = container.data( 'year' );
+				break;
+		}
 		page = 1;
 	} );
-	
+
 	function bindEvents() {
-		// todo
+		// todo base on scrolling/visibility of last loaded item
 		loadedIds = new Array();
 		$( '#primary' ).on( 'click keydown', '.load-more', function( e ) {
 			e.preventDefault();
-			loadPage( page, args );
+			loadPage();
 		});
 	}
 
-	function loadPage( page, args ) {
+	function loadPage() {
 		var params;
 		container.addClass( 'loading' );
 		params = {
 			'action': 'anndl-load-archive-posts',
+			'page': page,
 			'args': args
 		}
 		$.post( wp.ajax.settings.url, params, function( data ) {
 			data = $.parseJSON( data );
 			if ( data ) {
-				$.each( data, function ( post ) {
+				$.each( data, function ( id, post ) {
 					container.append( template( post ) );
 				});
 				page = page + 1;

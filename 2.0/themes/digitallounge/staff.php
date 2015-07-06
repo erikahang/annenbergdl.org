997e66a37155b9028364dc71acddc2ef6721ba41
[Mart]y <[stackm@usc.edu]>
2015-07-06 14:27:10 -0700

    styling of staff page

--- 2.0/themes/digitallounge/staff.php
+++ 2.0/themes/digitallounge/staff.php
@@ -13,7 +13,7 @@ wp_enqueue_script( 'digitallounge-staff', get_template_directory_uri() . '/js/st
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area staff-page">
 		<main id="main" class="site-main" role="main">
 
 	<?php 

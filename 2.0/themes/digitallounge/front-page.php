dc52272b78f652750086d7d37ebabe8eebbeb9b4
Nick Halsey <info@celloexpressions.com>
2015-07-08 21:53:06 -0700

    Revert to using the_excerpt() for tutorials on the home page

--- 2.0/themes/digitallounge/front-page.php
+++ 2.0/themes/digitallounge/front-page.php
@@ -77,9 +77,7 @@ get_header(); ?>
 											
 										</header><!-- .entry-header -->
 										<div class="entry-excerpt">
-											<?php $content = str_replace( ']]>', ']]&gt;', wpautop( get_post( $post->ID )->post_content ) );
-											echo substr( $content, 0, strpos( $content, '</p>' ) + 0 ) . '</p>';
-											?>
+											<?php the_excerpt(); ?>
 										</div><!-- .entry-excerpt -->
 									</article><!-- #tutorial-## -->
 								<?php } ?>

dae79252116c1484dfe432f1e74647ac3a26b16b
[Mart]y <[stackm@usc.edu]>
2015-07-21 14:51:30 -0700

    fixed ajax url for calendar

--- 2.0/themes/digitallounge/header.php
+++ 2.0/themes/digitallounge/header.php
@@ -25,7 +25,7 @@
 });
 </script>
 <script>$( document ).ready(function() {
-  	$( "#insert_events_calendar" ).load( "/annenbergdl.org/events/month #tribe-events-content" );
+  	$( "#insert_events_calendar" ).load( "./events/month #tribe-events-content" );
     
 });
 </script>

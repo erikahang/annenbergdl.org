41f34ba17f602642df79f8054feb6bca43ba5993
Nick Halsey <info@celloexpressions.com>
2015-07-23 18:24:51 -0700

    Add search form, implement the grid/archive view for search results

--- 2.0/themes/digitallounge/search.php
+++ 2.0/themes/digitallounge/search.php
@@ -1,17 +1,33 @@
 <?php
 /**
- * The template for displaying search results pages.
+ * The template for displaying archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package Digital Lounge
  */
 
+wp_enqueue_script( 'digitallounge-infinitescroll', get_template_directory_uri() . '/js/infinite-scroll.js', array( 'jquery', 'wp-util' ), '20150614', true );
+
+// Build data to replicate this query in JS/infinite scroll
+$data = array(
+	'page' => 1,
+	'type' => 'search',
+	'searchterm' => get_search_query(),
+);
+
+$alldata = '';
+foreach ( $data as $attr => $value ) {
+	$alldata .= 'data-' . esc_attr( $attr ) . '="' . esc_attr( $value ) . '" ';
+}
+
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main" data-type="tutorials">
 
 		<?php if ( have_posts() ) : ?>
-
+			<section class="query-container" <?php echo $alldata; ?>>
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'digitallounge' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
@@ -19,26 +35,73 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark" class="featured-image"><?php echo digitallounge_get_the_post_thumbnail(); ?></a>
+					<header class="entry-header">
+						<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+						<div class="entry-meta">
+							<?php digitallounge_posted_on(); ?>
+						</div><!-- .entry-meta -->
+					</header><!-- .entry-header -->
+
+					<div class="entry-excerpt">
+						<?php the_excerpt(); ?>
+					</div><!-- .entry-excerpt -->
+				</article><!-- #post-## -->
 
 			<?php endwhile; ?>
 
-			<?php the_posts_navigation(); ?>
-
+				<button class="load-more">Load More</button>
+			</section>
 		<?php else : ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-
+		<script type="text/html" id="tmpl-archive-post">
+			<article id="post-{{ data.id }}" class="post">
+				<a href="{{ data.permalink }}" rel="bookmark" class="featured-image">{{{ data.post_thumbnail }}}</a>
+				<header class="entry-header">
+					<h1 class="entry-title"><a href="{{ data.permalink }}" rel="bookmark">{{ data.title }}</a></h1>
+					<div class="entry-meta">
+						{{{ data.posted_on }}}
+					</div>
+				</header>
+				<div class="entry-excerpt">
+					{{{ data.excerpt }}}
+				</div>
+			</article>
+		</script>
+		<script type="text/html" id="tmpl-archive-tutorial">
+			<article id="post-{{ data.id }}" class="tutorials">
+				<a href="{{ data.permalink }}" rel="bookmark" class="featured-image">{{{ data.post_thumbnail }}}</a>
+				<header class="entry-header">
+					<h1 class="entry-title"><a href="{{ data.permalink }}" rel="bookmark">{{ data.title }}</a></h1>
+					<div class="entry-meta">
+						{{{ data.posted_on }}}
+					</div>
+				</header>
+				<div class="entry-excerpt">
+					{{{ data.excerpt }}}
+				</div>
+			</article>
+		</script>
+		<script type="text/html" id="tmpl-archive-grid-view">
+			<article id="post-{{ data.id }}" <?php post_class(); ?>>
+				<a href="{{ data.permalink }}" rel="bookmark" class="featured-image">{{{ data.post_thumbnail }}}</a>
+				<header class="entry-header">
+					<h1 class="entry-title"><a href="{{ data.permalink }}" rel="bookmark">{{ data.title }}</a></h1>
+					<div class="entry-meta">
+						{{{ data.posted_on }}}
+					</div>
+				</header>
+				<div class="entry-excerpt">
+					{{{ data.excerpt }}}
+				</div>
+			</article>
+		</script>
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>

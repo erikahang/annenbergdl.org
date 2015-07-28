eece7460068177b3a81094cabfacb18af3e72a29
Nick Halsey <info@celloexpressions.com>
2015-07-27 17:41:27 -0700

    Add general functions plugin; switch "Posts" to "News"

---
+++ 2.0/plugins/digital-lounge-functions.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name: Digital Lounge Functions
+ * Description: Custom functionality for the Digital Lounge site.
+ * Author: Nick Halsey
+ */
+
+/**
+ * Rename "Posts" to "News"
+ */
+function digitallounge_change_post_label() {
+	global $menu;
+	global $submenu;
+	$menu[5][0] = 'News';
+	$submenu['edit.php'][5][0] = 'News';
+	$submenu['edit.php'][10][0] = 'Add News';
+	$submenu['edit.php'][16][0] = 'News Tags';
+	echo '';
+}
+
+function digitallounge_change_post_object() {
+	global $wp_post_types;
+	$labels = &$wp_post_types['post']->labels;
+	$labels->name = 'News';
+	$labels->singular_name = 'News';
+	$labels->add_new = 'Add News';
+	$labels->add_new_item = 'Add News';
+	$labels->edit_item = 'Edit News';
+	$labels->new_item = 'News';
+	$labels->view_item = 'View News';
+	$labels->search_items = 'Search News';
+	$labels->not_found = 'No News found';
+	$labels->not_found_in_trash = 'No News found in Trash';
+	$labels->all_items = 'All News';
+	$labels->menu_name = 'News';
+	$labels->name_admin_bar = 'News';
+}
+
+add_action( 'admin_menu', 'digitallounge_change_post_label' );
+add_action( 'init', 'digitallounge_change_post_object' );

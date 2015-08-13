f3db51168452f4ade0a08f2a1808ff7b0c7e0290
Nick Halsey <info@celloexpressions.com>
2015-08-12 19:43:18 -0700

    Remove duplicate category display on news posts

--- 2.0/themes/digitallounge/inc/template-tags.php
+++ 2.0/themes/digitallounge/inc/template-tags.php
@@ -61,12 +61,6 @@ function digitallounge_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' == get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( __( ', ', 'digitallounge' ) );
-		if ( $categories_list && digitallounge_categorized_blog() ) {
-			printf( '<span class="cat-links">' . __( 'Posted in %1$s', 'digitallounge' ) . '</span>', $categories_list );
-		}
-
-		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', __( ', ', 'digitallounge' ) );
 		if ( $tags_list ) {
 			printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'digitallounge' ) . '</span>', $tags_list );
@@ -79,7 +73,7 @@ function digitallounge_entry_footer() {
 		echo '</span>';
 	}
 
-	edit_post_link( __( 'Edit', 'digitallounge' ), '<span class="edit-link">', '</span>' );
+	edit_post_link( __( 'Edit', 'digitallounge' ), '<span class="edit-link"> ', '</span>' );
 }
 endif;
 

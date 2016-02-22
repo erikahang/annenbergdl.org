e121e292e23976392642d4eb1dc40e4d72887ca0
Nick Halsey <info@celloexpressions.com>
2016-02-22 15:58:54 -0800

    Add google analytics tracking code
    This allows us to remove the unuseful plugin that does a similar thing
    while adding excessive admin UI.

--- 2.0/plugins/digital-lounge-functions.php
+++ 2.0/plugins/digital-lounge-functions.php
@@ -62,6 +62,22 @@ function digitallounge_activate_adjust_roles() {
 }
 register_activation_hook( __FILE__, 'digitallounge_activate_adjust_roles' );
 
+function digitallounge_google_analytics_tracking_code() {
+?>
+	<script type="text/javascript">
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','__gaTracker');
+
+		__gaTracker('create', 'UA-54263385-1', 'auto');
+		__gaTracker('set', 'forceSSL', true);
+		__gaTracker('require', 'displayfeatures');
+		__gaTracker('send','pageview');
+	</script>
+<?php
+}
+add_action( 'wp_head', 'digitallounge_google_analytics_tracking_code' );
 
 /**
  * Implement reuired publishing checklist items.

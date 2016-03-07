67a82fec0711b7407ae465cfcc7a3aab75752325
Nick Halsey <info@celloexpressions.com>
2016-03-07 14:30:50 -0800

    Show featured images on single pages

--- 2.0/themes/digitallounge/template-parts/content-page.php
+++ 2.0/themes/digitallounge/template-parts/content-page.php
@@ -9,6 +9,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_post_thumbnail( 'digitallounge-full-width' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">

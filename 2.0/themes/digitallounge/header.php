6f46d7bb774b4ed5e12a7f1fd8517024a2959ded
Nick Halsey <info@celloexpressions.com>
2016-05-02 12:48:24 -0700

    Clean up unnecessary code in document head

--- 2.0/themes/digitallounge/header.php
+++ 2.0/themes/digitallounge/header.php
@@ -17,41 +17,16 @@
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/buttontoggle.js"></script>
 <link href="<?php bloginfo('template_url'); ?>/css/animate.css" rel="stylesheet" type="text/css">
 
-
 <script>$( document ).ready(function() {
   	$( "#insert_events_calendar" ).load( "<?php echo site_url(); ?>/events/month #tribe-events-content");
 });
 </script>
-<link rel='stylesheet' id='tribe-events-bootstrap-datepicker-css-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/vendor/bootstrap-datepicker/css/datepicker.css?ver=4.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='tribe-events-custom-jquery-styles-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/vendor/jquery/smoothness/jquery-ui-1.8.23.custom.css?ver=4.2.2' type='text/css' media='all' />
-<link rel='stylesheet' id='tribe-events-full-calendar-style-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/src/resources/css/tribe-events-full.min.css?ver=3.10.1' type='text/css' media='all' />
-<link rel='stylesheet' id='tribe-events-calendar-style-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/src/resources/css/tribe-events-theme.min.css?ver=3.10.1' type='text/css' media='all' />
-<link rel='stylesheet' id='tribe-events-calendar-full-mobile-style-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/src/resources/css/tribe-events-full-mobile.min.css?ver=3.10.1' type='text/css' media='only screen and (max-width: 768px)' />
-<link rel='stylesheet' id='tribe-events-calendar-mobile-style-css'  href='<?php echo site_url(); ?>/wp-content/plugins/the-events-calendar/src/resources/css/tribe-events-theme-mobile.min.css?ver=3.10.1' type='text/css' media='only screen and (max-width: 768px)' />
+
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-
-<!-- Favicon and OS related icons -->
-<link rel="icon" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon.ico" type="image/x-icon" />
-<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon.ico" type="image/x-icon" />
-<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/img/favicon/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="<?php bloginfo('template_url'); ?>/img/favicon/manifest.json">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="msapplication-TileImage" content="<?php bloginfo('template_url'); ?>/img/favicon/mstile-144x144.png">
 <meta name="theme-color" content="#ffffff">
-
 
 <?php wp_head(); ?>
 </head>

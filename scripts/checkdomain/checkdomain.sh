5fd5c20237fb6db0de35cc4d0d128e2fa0916373
Nick Halsey <info@celloexpressions.com>
2016-09-29 14:18:50 -0700

    Remove unused/irrelevant files
    Some of this seems to be for other projects, and is presumably no longer
    needed.

--- scripts/checkdomain/checkdomain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-awk '{print $1}' < ascjportfoliolist.txt | while read ip; do
-    if ping -c1 $ip >/dev/null 2>&1; then
-        echo $ip IS UP
-    else
-        echo $ip IS DOWN
-    fi
-done
